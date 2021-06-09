fb64b9fb5a1bb859e24409566d5a7df60b3706a5
Joe Wuthrich <joerwuthrich@gmail.com>
2021-06-09 15:22:03 +1200

    Added room size and collisions visualization

--- src/main/java/com/joewuthrich/dungeongenerator/commands/DungeonGeneratorCMD.java
+++ src/main/java/com/joewuthrich/dungeongenerator/commands/DungeonGeneratorCMD.java
@@ -1,12 +1,10 @@
 package com.joewuthrich.dungeongenerator.commands;
 
 import com.joewuthrich.dungeongenerator.roomgenerator.Room;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 import static com.joewuthrich.dungeongenerator.placeblocks.PlaceBlocks.placeBlocks;
 import static com.joewuthrich.dungeongenerator.roomgenerator.GenerateRooms.generateRooms;
@@ -17,8 +15,6 @@ public class DungeonGeneratorCMD implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player))
             return false;
-
-        Player p = (Player) sender;
 
         Room[] roomList = generateRooms(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]),
                 Integer.parseInt(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]));
--- src/main/java/com/joewuthrich/dungeongenerator/placeblocks/PlaceBlocks.java
+++ src/main/java/com/joewuthrich/dungeongenerator/placeblocks/PlaceBlocks.java
@@ -12,21 +12,35 @@ public class PlaceBlocks {
 
         World w = Bukkit.getServer().getWorld("world");
         int[] coordinates;
+        int length;
+        int width;
 
         fillBlocks(w, seCorner, nwCorner);
 
         for (int i = 0; i < roomList.length; i++) {
             coordinates = roomList[i].getCoordinates();
+            length = roomList[i].getHeight();
+            width = roomList[i].getWidth();
 
-            w.getBlockAt(coordinates[0], 100, coordinates[1]).setType(Material.WHITE_CONCRETE);
+            for (int x = coordinates[0]; x < coordinates[0] + length; x++) {
+                for (int y = coordinates[1]; y < coordinates[1] + width; y++) {
+                    if (w.getBlockAt(x, 100, y).getType() == Material.BLACK_CONCRETE)
+                        w.getBlockAt(x, 100, y).setType(Material.WHITE_CONCRETE);
+                    else
+                        w.getBlockAt(x, 100, y).setType(Material.RED_CONCRETE);
+                }
+            }
         }
 
         return true;
     }
 
     public static boolean fillBlocks(World w, int[] seCorner, int[] nwCorner) {
-        for (int x = nwCorner[0]; x <= seCorner[0]; x++) {
-            for (int z = nwCorner[1]; z <= seCorner[1]; z++) {
+
+        int extraSize = 40;
+
+        for (int x = nwCorner[0] - extraSize; x <= seCorner[0] + extraSize; x++) {
+            for (int z = nwCorner[1] - extraSize; z <= seCorner[1] + extraSize; z++) {
                 w.getBlockAt(x, 100, z).setType(Material.BLACK_CONCRETE);
             }
         }
