0c8abf401d360afd5fd3bd3bb2295b74584b1f4d
Joe Wuthrich <joerwuthrich@gmail.com>
2021-06-09 13:37:06 +1200

    Initial commit

---
+++ src/main/java/com/joewuthrich/dungeongenerator/dungeongenerator/DungeonGenerator.java
@@ -0,0 +1,17 @@
+package com.joewuthrich.dungeongenerator.dungeongenerator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DungeonGenerator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
