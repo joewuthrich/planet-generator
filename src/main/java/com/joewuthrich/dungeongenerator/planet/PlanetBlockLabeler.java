528089ab757fe3cc99e2e542cd4a02ad957e6021
Joe Wuthrich <joerwuthrich@gmail.com>
2021-06-14 00:54:43 +1200

    Added not null checks

--- src/main/java/com/joewuthrich/dungeongenerator/planet/PlanetBlockLabeler.java
+++ src/main/java/com/joewuthrich/dungeongenerator/planet/PlanetBlockLabeler.java
@@ -4,24 +4,22 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PlanetBlockLabeler {
     public static void labelBlock(List<Block> blocks) {
-        List<Block> topBlocks = new ArrayList<>();
-
         Material overlay = Material.BLACK_CONCRETE_POWDER,
                 underlay = Material.BLACK_WOOL;
 
         for (Block b : blocks) {
-            if (Bukkit.getServer().getWorld("world").getBlockAt(b.getX(), b.getY() + 1, b.getZ()).getType() == Material.AIR) {
+            if (Objects.requireNonNull(Bukkit.getServer().getWorld("world")).getBlockAt(b.getX(), b.getY() + 1, b.getZ()).getType() == Material.AIR) {
                 b.setType(overlay);
             }
         }
 
         for (Block b : blocks) {
-            if (Bukkit.getServer().getWorld("world").getBlockAt(b.getX(), b.getY() + 1, b.getZ()).getType() == overlay) {
+            if (Objects.requireNonNull(Bukkit.getServer().getWorld("world")).getBlockAt(b.getX(), b.getY() + 1, b.getZ()).getType() == overlay) {
                 b.setType(underlay);
             }
         }
